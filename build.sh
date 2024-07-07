035d1f8b8b76c3d36a210dc442316b7357805d2a
Jan Schiefer <janschiefer@users.noreply.github.com>
2024-07-07 02:03:32 +0200

    FIFO 1

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ rm -f *.json *.fs *.log hw/gen/*.v hw/gen/*.vhd
 sbt "runMain MyUART.MyTopLevelHWVerilog"
 # -retime fails to work, -noalu sometimes needed
 yosys -DSYNTHESIS -l yosys.log -p "read_verilog -sv hw/gen/*.v; synth_gowin -top MyTopLevelHW  -abc9 -json build.yosys.json" 
-#nextpnr-gowin --enable-globals --enable-auto-longwires --parallel-refine --placer-heap-cell-placement-timeout 0  --placer-heap-timingweight 30 --json build.yosys.json --write build.nextpnr.json --device GW1NR-LV9QN88PC6/I5 --family GW1N-9C --cst tangnano9k.cst --freq 27 --timing-allow-fail -l nextpnr.log
-nextpnr-himbaechel --detailed-timing-report --json build.yosys.json --write build.nextpnr.json --device GW1NR-LV9QN88PC6/I5 --vopt family=GW1N-9C --vopt cst=tangnano9k.cst
+nextpnr-gowin --enable-globals --enable-auto-longwires --parallel-refine --placer-heap-cell-placement-timeout 0  --placer-heap-timingweight 30 --json build.yosys.json --write build.nextpnr.json --device GW1NR-LV9QN88PC6/I5 --family GW1N-9C --cst tangnano9k.cst --freq 27 --timing-allow-fail -l nextpnr.log
+#nextpnr-himbaechel --detailed-timing-report --json build.yosys.json --write build.nextpnr.json --device GW1NR-LV9QN88PC6/I5 --vopt family=GW1N-9C --vopt cst=tangnano9k.cst
 gowin_pack --done_as_gpio -d GW1N-9C -o bitstream.fs build.nextpnr.json
 openFPGALoader --verbose --board tangnano9k --bitstream bitstream.fs
--- hw/spinal/MyUART/MyTopLevel.scala
+++ hw/spinal/MyUART/MyTopLevel.scala
@@ -10,40 +10,33 @@ case class MyTopLevel() extends Component {
     val txData = out Bool ()
   }
 
-  val writeStream = Stream(Bits(8 bits))
+  val message = B"h48454C4C4F0D0A" // HELLO\CR\LF
 
-  writeStream.payload := B("10000101")
+  val push_counter = RegInit(U(0, 8 bits))
 
-  writeStream.valid := False
+  val input_fifo = new StreamFifo(
+        dataType = Bits(8 bit),
+        depth = 32
+      )
 
-  val fsm = new StateMachine {
+  val pushStream, popStream = Stream(Bits(8 bits))
 
-    val WAIT: State = new StateDelay(1000) with EntryPoint {
-      whenCompleted {
-        goto(SEND)
-      }
-    }
+  input_fifo.io.push << pushStream
+  input_fifo.io.pop >> popStream
 
-    val SEND: State = new State {
-      onEntry { writeStream.valid := True }
-      whenIsActive {
-        writeStream.valid := True
-        when(!writeStream.ready) { goto(WAIT_FINISH_SEND) }
-      }
-    }
+  pushStream.valid := True
+  pushStream.payload := (message  |>> (48 - push_counter))( 7 downto 0)
 
-    val WAIT_FINISH_SEND: State = new State {
-      whenIsActive {
-        when(writeStream.ready) { goto(WAIT) }
-      }
-    }
+  when(pushStream.ready.fall()) {
 
-  }
+   push_counter := push_counter + U(8).resized
+  
+   }
 
   val send = MyUARTSend(234, 8, 2, UartParity.ODD) // 234 for 115200 bps
 
   io.txData <> send.io.txData
-  send.io.sendDataStream <> writeStream
+  popStream >> send.io.sendDataStream
 }
 
 object MyTopLevelVerilog extends App {
