6882e9ee34a3adf6132b0ba530f03f8b543e9047
Jan Schiefer <janschiefer@users.noreply.github.com>
2024-07-10 01:16:25 +0200

    Formal

--- hw/spinal/MyUART/MyTopLevelFormal.scala
+++ hw/spinal/MyUART/MyTopLevelFormal.scala
@@ -2,26 +2,56 @@ package MyUART
 
 import spinal.core._
 import spinal.core.formal._
+import spinal.lib._
+import spinal.lib.formal._
 
 // You need SymbiYosys to be installed.
 // See https://spinalhdl.github.io/SpinalDoc-RTD/master/SpinalHDL/Formal%20verification/index.html#installing-requirements
 object MyTopLevelFormal extends App {
 
   // CHECK 1: Line should be always high when NOT busy aka idle
-  FormalConfig
-    .withProve(50)
-    .doVerify(new Component {
-      val dut = FormalDut(MyUARTSend(2, 8, 2, UartParity.ODD))
 
-      assumeInitial(clockDomain.isResetActive)
+    FormalConfig
+      .withProve(50)
+      .doVerify(new Component {
+        val dut = FormalDut(MyUARTSend(2, 8, 2, UartParity.ODD))
 
-      anyseq(dut.io.uartDataStream.payload)
+        val reset = ClockDomain.current.isResetActive
 
-      dut.io.uartDataStream.valid := False
+        assumeInitial(clockDomain.isResetActive)
 
-      assert(dut.io.txData)
+        when(reset || past(reset)) {
+          assume(dut.io.uartDataStream.valid === False)
+        }
 
-    })
+        anyseq(dut.io.uartDataStream.payload)
+
+        dut.io.uartDataStream.valid := False
+
+        assert(dut.io.txData)
+
+      })
+
+
+    FormalConfig
+      .withProve(50)
+      .doVerify(new Component {
+        val dut = FormalDut(MyUARTSend(2, 8, 2, UartParity.ODD))
+
+        val reset = ClockDomain.current.isResetActive
+
+        assumeInitial(clockDomain.isResetActive)
+
+        anyseq(dut.io.uartDataStream.payload)
+
+        dut.io.uartDataStream.valid := True
+
+        when(pastValid() && dut.io.uartDataStream.ready.rise) {
+            assert(past(dut.io.txData)) //STOP bit 2
+            assert(past(dut.io.txData,2)) //Stop bit 1
+        }
+
+      })
 
   /*
 
