58e2b1c0f481c4ff90a3a146a3e912805d37e047
Jan Schiefer <janschiefer3@users.noreply.github.com>
2024-07-30 23:05:00 +0200

    Formalpy verify bytes

--- hw/spinal/MyUART/MyTopLevelFormal.scala
+++ hw/spinal/MyUART/MyTopLevelFormal.scala
@@ -9,13 +9,15 @@ import spinal.core.formal._
 // See https://spinalhdl.github.io/SpinalDoc-RTD/master/SpinalHDL/Formal%20verification/index.html#installing-requirements
 object MyTopLevelFormal extends App {
 
+  val bitsPerTransfer = 8
+  val stopBits = 2
   // CHECK 1: Line should be always high when NOT busy aka idle
   FormalConfig
     .withBMC(50)
     .withProve(50)
     .withCover(50)
     .doVerify(new Component {
-      val dut = FormalDut(MyUARTSend(1, 8, 2, UartParity.ODD))
+      val dut = FormalDut(MyUARTSend(1, bitsPerTransfer, stopBits, UartParity.ODD))
 
       val reset = ClockDomain.current.isResetActive
 
@@ -33,11 +35,12 @@ object MyTopLevelFormal extends App {
 
     })
 
-  // Test STOP bits
   FormalConfig
+    .withBMC(50)
     .withProve(50)
+    .withCover(50)
     .doVerify(new Component {
-      val dut = FormalDut(MyUARTSend(1, 8, 2, UartParity.ODD))
+      val dut = FormalDut(MyUARTSend(1, bitsPerTransfer, stopBits, UartParity.ODD))
 
       val reset = ClockDomain.current.isResetActive
 
@@ -47,9 +50,19 @@ object MyTopLevelFormal extends App {
 
       dut.io.uartDataStream.valid := True
 
+      when(!dut.io.uartDataStream.ready) {
+        assume(stable(dut.io.uartDataStream.payload))
+      }
+
       when(pastValid() && dut.io.uartDataStream.ready.rise) {
-        assert(past(dut.io.txData, 1)) // STOP bit 2
-        assert(past(dut.io.txData, 2)) // STOP bit 1
+
+        for (i <- 1 to stopBits) {
+          assert(past(dut.io.txData, i))
+        }
+
+        for (i <- 0 to (bitsPerTransfer - 1)) {
+          assert(past(dut.io.txData, 4 + i) === past(dut.io.uartDataStream.payload(bitsPerTransfer - 1 - i), 1))
+        }
       }
 
     })
